54ddfa2e4de72b97c016d48f45a84e1b6128c191
Kwarthys <Kwarthys@gmail.com>
2025-12-05 21:55:16 +0100

    No more rendering of dead enemies and light now emits shadows

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -22,17 +22,11 @@ public partial class GameManager : Node
 	{
 		int n = 1000;
 		enemyManager.Initialize(n);
-		multiMesh.Multimesh.InstanceCount = n;
 
 		tree = new(new(-25.0f, -25.0f, 50.0f, 50.0f), enemyManager);
 
 		for(int i = 0; i < enemyManager.count; ++i)
-		{
-			Vector3 pos = new(enemyManager.positions[i].X, 0.5f, enemyManager.positions[i].Y);
-			multiMesh.Multimesh.SetInstanceTransform(i, new Transform3D(Basis.Identity, pos));
-
-			tree.SubmitElement(i, enemyManager.positions[i]);
-		}
+			tree.SubmitElement(i, enemyManager.GetPosition(i));
 
 		buildingsManager.Initialize(enemyManager, tree);
 		buildingsManager.LoadData(BuildingsDataPath);
@@ -43,6 +37,7 @@ public partial class GameManager : Node
 			turret.Position = new(b.position.X, 0.0f, b.position.Y);
 			AddChild(turret);
 		}
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -61,15 +56,22 @@ public partial class GameManager : Node
 		if(drawTreeDebug)
 			tree.DrawDebug();
 
+		int meshesToDisplay = 0;
 		for(int i = 0; i < enemyManager.count; ++i)
 		{
-			Vector3 pos;
-			if(enemyManager.healths[i] > 0.0)
-				pos = new(enemyManager.positions[i].X, 0.5f, enemyManager.positions[i].Y);
-			else
-				pos = new(0.0f, -1.0f, 0.0f); // Dead, hide it below ground
+			if(enemyManager.Alive(i))
+				meshesToDisplay++;
+		}
+		multiMesh.Multimesh.InstanceCount = meshesToDisplay;
 
-			multiMesh.Multimesh.SetInstanceTransform(i, new Transform3D(Basis.Identity, pos));
+		int meshIndex = 0;
+		for(int i = 0; i < enemyManager.count; ++i)
+		{
+			if(enemyManager.Alive(i) == false)
+				continue;
+
+			Vector3 pos = new(enemyManager.positions[i].X, 0.5f, enemyManager.positions[i].Y);
+			multiMesh.Multimesh.SetInstanceTransform(meshIndex++, new Transform3D(Basis.Identity, pos));
 		}
 
 		dtCounter += _dt;
